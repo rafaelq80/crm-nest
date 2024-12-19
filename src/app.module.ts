1311032eff43bc99524c1e990ed55bce8a324f03
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2024-12-19 06:04:46 -0300

    :rocket: Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { UsuarioModule } from './usuario/usuario.module';
-import { Usuario } from './usuario/entities/usuario.entity';
-import { Cliente } from './cliente/entities/cliente.entity';
-import { Oportunidade } from './oportunidade/entities/oportunidade.entity';
-import { OportunidadeModule } from './oportunidade/oportunidade.module';
 import { ClienteModule } from './cliente/cliente.module';
+import { ProdService } from './data/services/prod.service';
+import { OportunidadeModule } from './oportunidade/oportunidade.module';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_crm',
-      entities: [Oportunidade, Cliente, Usuario],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
     OportunidadeModule,
     ClienteModule,
---
+++ src/data/services/dev.service.ts
@@ -0,0 +1,21 @@
+﻿import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Usuario } from '../../usuario/entities/usuario.entity';
+import { Cliente } from '../../cliente/entities/cliente.entity';
+import { Oportunidade } from '../../oportunidade/entities/oportunidade.entity';
+
+@Injectable()
+export class DevService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_crm',
+      entities: [Oportunidade, Cliente, Usuario],
+      synchronize: true,
+    };
+  }
+}
